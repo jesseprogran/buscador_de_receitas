1891af4d6cbdaae81514c7ec8ed927a1e7b38ba4
Jessé <jessepescador22@gmail.com>
2024-10-01 23:18:58 -0300

    realizando ajuste e finalizando

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 const recipeList = document.querySelector(".recipe-list");
+const recipeDetails = document.querySelector(".recipe-details");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,12 +10,17 @@ form.addEventListener("submit", (e) => {
 });
 
 async function searchRecipes(ingredient) {
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
-  );
+  recipeList.innerHTML = `<p>Carregando....</p>`;
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
 
-  const data = await response.json();
-  showRecipes(data.meals);
+    const data = await response.json();
+    showRecipes(data.meals);
+  } catch (err) {
+    recipeList.innerHTML = `<p>Nenhuma receita encontrada</p>`;
+  }
 }
 
 function showRecipes(recipes) {
@@ -23,7 +29,7 @@ function showRecipes(recipes) {
       (item) => `
     
 
-    <div class="recipe-card">
+    <div class="recipe-card" onclick="getRecipesDetails(${item.idMeal})"> 
 
     <img src="${item.strMealThumb}" alt="receita foto">
     <h3>${item.strMeal}</h3>
@@ -33,4 +39,39 @@ function showRecipes(recipes) {
     `
     )
     .join("");
+}
+
+async function getRecipesDetails(id) {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+
+  const data = await response.json();
+  const recipe = data.meals[0];
+
+  let ingredients = "";
+
+  for (let i = 1; i <= 20; i++) {
+    if (recipe[`strIngredient${i}`]) {
+      ingredients += `<li>${recipe[`strIngredient${i}`]} - ${
+        recipe[`strMeasure${i}`]
+      }</li>`;
+    } else {
+      break;
+    }
+  }
+
+  recipeDetails.innerHTML = `
+
+    <h2>${recipe.strMeal}</h2>
+    <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" class="recipe-img">
+    <h3>Categoria:${recipe.strCategory}</h3>
+    <h3>Origem:${recipe.strArea}</h3>
+    <h3>Igredientes:</h3>
+    <ul>${ingredients}</ul>
+    <h3>Instruções:</h3>
+    <p>${recipe.strInstructions}</p>
+    <p>Tags:${recipe.strTags}</p>
+    <p>Vídeo: <a href="${recipe.strYoutube}" target="_blank">Assista no Youtube</a></p>
+  `;
 }
